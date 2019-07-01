a6a974cbf510af74dfb6a69255f65626c3faf65f
Alvis Jansons <alvis.jansons@inbox.lv>
2019-07-01 08:28:35 +0100

    Added Register folder and register menu and onRoutechanger and onClick to go from sign in to signout and register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Clarifai from 'clarifai'; /* need to add because npm install clarify and 
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js'; /* background particles walking arround*/
@@ -30,9 +31,30 @@ class App extends React.Component {
     super();
     this.state = { /* to remember entered value and replace when new entered*/
       input : '',
-      imageUrl:''
-    }
+      imageUrl:'',
+      box:{},
+      route: 'signin' /* route is fallowing up where you are on page*/
+    } 
   }
+
+calculateFaceLocation = (data) => {/* to create box arround face wi taking parameters getting from clarify api*/
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');/* imputimage is from faceRecognition.js file and it means url gived in input box*/
+  const width = Number(image.width); /* getting image sizes*/
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: height * (clarifaiFace.bottom_row * height),
+    bottomRow: height * (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFacebox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
 
   onInputChange = (event) => {
     this.setState({input: event.target.value}); /*to fallow up the event and target.value to get acess to ImageLinkForm.js onInputChange*/
@@ -41,36 +63,52 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     console.log('click');/* copy app.models.... from clarify website*/
-    this.setState({imageUrl: this.state.input}) /*  on click going to from inputed url to Facerecognition*/
+    this.setState({imageUrl: this.state.input}); /*  on click going to from inputed url to Facerecognition*/
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
           // URL
           this.state.input /*to put in api input url and recogniting face*/
-      ).then(
-    function(response) {
+      ).then(response => this.displayFacebox(this.calculateFaceLocation(response)))
+      
       // do something with response
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);/*info from google chrome development tool place*/
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+      /* console.log(response.outputs[0].data.regions[0].region_info.bounding_box); info from google chrome development tool place 4 coordinates to get arround face*/
+      .catch(err => console.log(err));
   }
+
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
   render(){
     return (
     <div className="App">
       <Particles className="particles"
               params={particlesOption}  
             />
-      <Navigation/>
-      <Signin/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit ={this.onButtonSubmit}/>{/* Showing value from ImageLinkForm.js onInputChange to here onInputChange = (event) => {
-    console.log(event);
-}  and showing up here after rank*/} {/* onButtonSubmit ={this.onButtonSubmit} to get access to onButtonSubmit. When clicking Detect*/}
-      {<FaceRecognition imageUrl = {this.state.imageUrl}/>}{/* place where picture will be displayed.  {this.state.imageUrl} is taking url from input and goes here and in js file. */}
-
+      <Navigation onRouteChange = {this.onRouteChange}/>
+      {this.state.route === 'home' 
+          ? <div>
+                <Logo/>
+                <Rank/>
+                <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit ={this.onButtonSubmit}
+                />
+                {/* Showing value from ImageLinkForm.js onInputChange to here onInputChange = (event) => 
+              console.log(event) and showing up here after rank*/} {/* onButtonSubmit ={this.onButtonSubmit} to get access to onButtonSubmit. When clicking Detect*/}
+                <FaceRecognition box={this.state.box} imageUrl = {this.state.imageUrl}/>
+            </div>
+          : (
+            this.state.route === 'signin'
+            ? <Signin onRouteChange= {this.onRouteChange}/>
+            : <Register onRouteChange= {this.onRouteChange}/>
+          )
+      }
+      
+      
+      
+      {/* place where picture will be displayed.  {this.state.imageUrl} is taking url from input and goes here and in js file. */}
     </div>
   );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 
 /* sing out navigation to sing in and out*/
-const FaceRecognition = ({imageUrl}) => {/* {imageUrl} to get to src*/
+const FaceRecognition = ({imageUrl, box}) => {/* {imageUrl} to get to src*/
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img  alt="" src={imageUrl} width='500' heig='auto'/>{/* imageUrl from Input to imageUrl and to  FaceRecognition and here */}
+                <img  alt=""  id='inputimage' src={imageUrl} width='500' heig='auto'/>{/* imageUrl from Input to imageUrl and to  FaceRecognition and here */}
+            <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
 }
 
-export default FaceRecognition;
+export default FaceRecognition; 
